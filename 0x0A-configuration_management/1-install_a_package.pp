27a6e3352e4daafd94bd7fe2b61b0e5885f172d6
stevovenom <stephenokoth2002@gmail.com>
2024-07-01 04:17:32 -0700

    Am using puppet to install flask from pip3

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,19 @@
+# installation flask from pip3
+
+package { 'python3-pip':
+	ensure => installed,
+}
+
+package { 'virtualenv':
+	ensure => installed,
+}
+
+package { 'python3-venv':
+	ensure => installed,
+}
+
+exec { 'install_flask':
+	command => '/usr/bin/pip3 install Flask',
+	path => ['/usr/bin'],
+	require => Package['python3-pip'],
+}
