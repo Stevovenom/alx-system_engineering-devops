7fc112e1ef86c87eed9c395fcd0a0fb7bfacf1ea
stevovenom <stephenokoth2002@gmail.com>
2024-07-01 04:24:40 -0700

    changes to the puppet-lint requirements

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,19 +1,19 @@
 # installation flask from pip3
 
 package { 'python3-pip':
-	ensure => installed,
+  ensure => installed,
 }
 
 package { 'virtualenv':
-	ensure => installed,
+  ensure => installed,
 }
 
 package { 'python3-venv':
-	ensure => installed,
+  ensure => installed,
 }
 
 exec { 'install_flask':
-	command => '/usr/bin/pip3 install Flask',
-	path => ['/usr/bin'],
-	require => Package['python3-pip'],
+  command => '/usr/bin/pip3 install Flask',
+  path    => ['/usr/bin'],
+  require => Package['python3-pip'],
 }
