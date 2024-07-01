39a89a29cffdf8b037539e86e6fe031626bddf56
stevovenom <stephenokoth2002@gmail.com>
2024-07-01 04:43:08 -0700

    update

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,4 @@
 # Install an especific version of flask (2.1.0)
-package {'Flask':
-  ensure   => '2.1.0',
-  provider => 'pip3'
+exec { 'puppet-lint':
+  command => '/usr/bin/apt-get -y install puppet-lint -v 2.5.0',
 }
