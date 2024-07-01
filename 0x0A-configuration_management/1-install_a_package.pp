c46adf7a89b0f86c95ac1132c43af03b7b61373e
stevovenom <stephenokoth2002@gmail.com>
2024-07-01 04:59:23 -0700

    error conflict

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,4 +1,7 @@
-# Install an especific version of flask (2.1.0)
-exec { 'puppet-lint':
-  command => '/usr/bin/apt-get -y install puppet-lint -v 2.5.0',
+# 1-install_a_package.pp
+
+# Install Flask version 2.1.0 using pip3
+package { 'Flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
